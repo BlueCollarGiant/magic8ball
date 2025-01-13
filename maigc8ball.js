177371d5f87eaaa5bf50e89cfd6807a416d941c0
BlueCollarGiant <167778887+BlueCollarGiant@users.noreply.github.com>
2025-01-13 16:23:17 -0600

    grabbing foundation

---
+++ maigc8ball.js
@@ -0,0 +1,25 @@
+let question = " setup user input here"
+
+let magic = Math.floor(Math.random()*10);
+
+console.log("What is your question? :" + question)
+
+if (magic === 0) {
+  console.log("yes");
+} else if (magic === 1) {
+  console.log("maybe");
+} else if (magic === 2) {
+  console.log("ask again later");
+} else if (magic === 3) {
+  console.log("why are you asking me");
+} else if (magic === 4) {
+  console.log("ugh you again");
+} else if (magic === 5) {
+  console.log("im leaning yes");
+} else if (magic === 6) {
+  console.log(" im leaning n");
+} else if (magic === 7) {
+  console.log("idk flip a coinn");
+} else if (magic === 8) {
+  console.log("sure why not");
+}
